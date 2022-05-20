ed086d25fbf608028a6238b5b5dee19e99f34dbc
Fahad Altaf <fahadaltaf10SE49@hotmail.com>
2022-05-20 07:11:07 +0500

    proxy added

--- hctax.net-scraper/Program.cs
+++ hctax.net-scraper/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace hctax.net_scraper
@@ -46,24 +47,46 @@ namespace hctax.net_scraper
         public List<Record> Records { get; set; } = new List<Record>();
         public int TotalRecordCount { get; set; }
     }
+    public class ProxyServer
+    {
+        public string proxy { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+        public string connectionType { get; set; }
+        public string asn { get; set; }
+        public string isp { get; set; }
+        public string type { get; set; }
+        public int lastChecked { get; set; }
+        public bool get { get; set; }
+        public bool post { get; set; }
+        public bool cookies { get; set; }
+        public bool referer { get; set; }
+        public bool userAgent { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string randomUserAgent { get; set; }
+        public int requestsRemaining { get; set; }
+    }
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
             List<DataModel> entries = ReadInputFile();
-            int num = 2;
+            int num = 10;
             int pages = (entries.Count + num - 1) / num;
             List<Task> tasks = new List<Task>();
             for (int count = 1; count <= pages; ++count)
             {
                 int index = count - 1;
                 var data = entries.Skip(index * num).Take(num).ToList();
-               
+
                 Task newTask = Task.Factory.StartNew(() => { ProcessRecord(data).Wait(); });
                 tasks.Add(newTask);
 
-                if (count % 10 == 0 || count == pages)
+                if (count % 20 == 0 || count == pages)
                 {
                     // Thread.Sleep(2000);
                     foreach (Task task in tasks)
@@ -71,6 +94,14 @@ namespace hctax.net_scraper
                         while (!task.IsCompleted)
                         { }
                     }
+                }
+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+                using (ExcelPackage excel = new ExcelPackage())
+                {
+                    excel.Workbook.Worksheets.Add("Products").Cells[1, 1].LoadFromCollection(entries, true);
+                    excel.SaveAs(new FileInfo("results.xlsx"));
                 }
             }
 
@@ -85,15 +116,37 @@ namespace hctax.net_scraper
             Console.ReadKey();
         }
 
+        public static WebProxy GetWebProxy()
+        {
+            WebProxy proxy;
+            try
+            {
+                proxy = new WebProxy("zproxy.lum-superproxy.io", 22225)
+                {
+                    Credentials = new NetworkCredential("lum-customer-hl_84bcc60a-zone-data_center-country-us", "")
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error:" + ex.Message);
+            }
+            return proxy;
+        }
+
         private static async Task ProcessRecord(List<DataModel> model)
         {
+            var proxy = GetWebProxy();
             foreach (var data in model)
             {
                 string messages = "";
                 try
                 {
-                    messages+="Processed address: " + data.Address+"\n";
-                    var client = new RestClient("https://www.hctax.net/Property/Actions/AccountsList?jtStartIndex=0&jtPageSize=20&jtSorting=Name%20ASC");
+                    messages += "Processed address: " + data.Address + "\n";
+                    RestClientOptions options = new RestClientOptions("https://www.hctax.net/Property/Actions/AccountsList?jtStartIndex=0&jtPageSize=20&jtSorting=Name%20ASC")
+                    {
+                        Proxy = proxy
+                    };
+                    var client = new RestClient(options);
                     var request = new RestRequest();
                     request.Method = Method.Post;
                     request.AlwaysMultipartFormData = true;
@@ -102,47 +155,79 @@ namespace hctax.net_scraper
                     var response = await client.ExecuteAsync<RootInit>(request);
                     if (response != null && response.Data != null)
                     {
-                        if (response.Data.TotalRecordCount == 1)
+                        if (response.Data.TotalRecordCount == 0)
                         {
-                            string id = await GetEncryptedId(response.Data.Records.FirstOrDefault().Account);
+                            Console.WriteLine("No result found");
+                        }
+                        else if (response.Data.TotalRecordCount == 1)
+                        {
+                            string id = await GetEncryptedId(response.Data.Records.FirstOrDefault().Account, proxy);
                             id = id.Replace("\r\n", "");
                             if (!string.IsNullOrEmpty(id))
                             {
                                 HtmlWeb web = new HtmlWeb();
-                                HtmlDocument doc = web.Load($"https://www.hctax.net/Property/TaxStatement?account={id}");
+                                HtmlDocument doc = web.Load($"https://www.hctax.net/Property/TaxStatement?account={id}", "GET", proxy, new NetworkCredential("lum-customer-hl_84bcc60a-zone-data_center-country-us", ""));
 
                                 var node = doc.DocumentNode.SelectNodes("//tr").FirstOrDefault(x => x.InnerText.Contains("Total Amount Due"));
                                 if (node != null)
                                 {
                                     var parts = node.InnerText.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
                                     data.Amount = parts[1].Trim();
-                                   messages+="Total Due: " + data.Amount+"\n";
+                                    messages += "Total Due: " + data.Amount + "\n";
                                 }
                             }
                             else
-                                messages+="We cant get the details because we are unable to get the encrypted id"+"\n";
+                                messages += "We cant get the details because we are unable to get the encrypted id" + "\n";
                         }
                         else
                         {
-                            messages+="Found multiple records: " + response.Data.TotalRecordCount+"\n";
-                            //foreach (var record in response.Data.Records)
-                            //{
+                            messages += "Found multiple records: " + response.Data.TotalRecordCount + "\n";
+                            foreach (var record in response.Data.Records)
+                            {
+                                if (record.Name.ToLower().Contains(data.LastName.ToLower()))
+                                {
+                                    string id = await GetEncryptedId(record.Account, proxy);
+                                    id = id.Replace("\r\n", "");
+                                    if (!string.IsNullOrEmpty(id))
+                                    {
+                                        HtmlWeb web = new HtmlWeb();
+                                        HtmlDocument doc = web.Load($"https://www.hctax.net/Property/TaxStatement?account={id}", "GET", proxy, new NetworkCredential("lum-customer-hl_84bcc60a-zone-data_center-country-us", ""));
 
-                            //}
+                                        var node = doc.DocumentNode.SelectNodes("//tr").FirstOrDefault(x => x.InnerText.Contains("Total Amount Due"));
+                                        if (node == null)
+                                        {
+                                            node = doc.DocumentNode.SelectNodes("//tr").FirstOrDefault(x => x.InnerText.Contains("Total Due >>>"));
+                                        }
+                                        if (node != null)
+                                        {
+                                            var parts = node.InnerText.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Where(x => x != "                            ").ToList();
+                                            data.Amount = parts[1].Trim();
+                                            messages += "Total Due: " + data.Amount + "\n";
+                                        }
+                                    }
+                                    else
+                                        messages += "We cant get the details because we are unable to get the encrypted id" + "\n";
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
                 catch (Exception ex)
                 {
-                    messages+="We are unable to process address. Reason: " + ex.Message+"\n";
+                    messages += "We are unable to process address. Reason: " + ex.Message + "\n";
                 }
                 Console.WriteLine(messages);
             }
         }
 
-        private static async Task<string> GetEncryptedId(string accountNumber)
+        private static async Task<string> GetEncryptedId(string accountNumber, WebProxy proxy)
         {
-            var client = new RestClient($"https://www.hctax.net/Property/AccountEncrypt?account={accountNumber}");
+            RestClientOptions options = new RestClientOptions($"https://www.hctax.net/Property/AccountEncrypt?account={accountNumber}")
+            {
+                Proxy = proxy
+            };
+            var client = new RestClient(options);
             var request = new RestRequest();
             request.Method = Method.Get;
             var response = await client.ExecuteAsync(request);
